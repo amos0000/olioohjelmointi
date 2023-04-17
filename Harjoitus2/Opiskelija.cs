e0167031ab116b529ea1041e521d83d56f87d7fa
Amos0000 <amos.ingman@student.careeria.fi>
2023-04-17 12:50:18 +0300

    opiskelija-luokka lisätty

---
+++ Harjoitus2/Opiskelija.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus2
+{
+    internal class Opiskelija
+    {
+        public string Nimi;
+    }
+}
